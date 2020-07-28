282f7a0a15ff4d42408dc04f128db502deb48849
ThiagoRAD <thauroo@gmail.com>
2020-07-28 19:42:09 -0300

    user form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,3 +1,19 @@
-export const Header = () => {
+import React, { useState, useContext } from 'react';
+import { UserContext } from './UserContext';
 
+export default function UserForm() {
+  const [inputName, setInputName] = useState('');
+  const { setName } = useContext(UserContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName(inputName);  // Set the name in context
+    window.history.pushState({}, '', '/quiz');  // Change the URL without reloading the page
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);  // Dispatch a navigation event
+  }
+
+  return (
+    // Add the form here
+  );
 }
