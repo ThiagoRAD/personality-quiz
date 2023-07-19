c3a3a2ee229830dae1a57fc346f73d1274917416
ThiagoRAD <thauroo@gmail.com>
2023-07-19 15:59:47 -0300

    context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
+import './Header.css'
 
 export const Header = () => {
-  return <header>
+  return <header className="header">
     <h1>Which element Are You?</h1>
     <p>(based on completely random things)</p>
     <nav>
